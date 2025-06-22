3651e9404dc3581de9a3228abef228d4c6a3e794
zvbrsten <zvbrsten@gmail.com>
2025-06-23 02:02:36 +0530

    signup page css fixed

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -73,7 +73,7 @@ const SignUp = ({ setCurrentPage }) => {
     }
   };
 
-  return <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+  return <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center bg-white">
       <h3 className="text-lg font-semibold text-black">Create an Account</h3>
       <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Join us by entering your details below.
