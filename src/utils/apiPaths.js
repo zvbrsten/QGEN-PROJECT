3515726cf2760e867d58e24276e1b9c487e7ece2
zvbrsten <zvbrsten@gmail.com>
2025-06-23 01:59:14 +0530

    first commit backedn added

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://qgen-project-backend-git-main-zvbrstens-projects.vercel.app/";
 
 export const API_PATHS = {
   AUTH: {
